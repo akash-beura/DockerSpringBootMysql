1f7dcf641dd9b096dca4360ee708c6eb519712a8
M1049006 <akash.beura@mindtree.com>
2020-05-25 14:55:05 +0530

    Dockerfile fixes

--- src/main/java/com/mindtree/dockerproject/entity/Person.java
+++ src/main/java/com/mindtree/dockerproject/entity/Person.java
@@ -1,12 +1,17 @@
 package com.mindtree.dockerproject.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="person_demo")
 public class Person {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int personId;
 	private String personName;
 
